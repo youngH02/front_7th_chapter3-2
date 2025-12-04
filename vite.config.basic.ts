59391b7c89353c8497f04fddf5eb0d43446dec19
jyoung02up <>
2025-12-04 14:33:27 +0900

    배포파일 업로드

---
+++ vite.config.basic.ts
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+
+export default mergeConfig(
+  defineConfig({
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: "index.basic.html",
+      },
+      outDir: "dist",
+    },
+    base: "./",
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
+    },
+  })
+);
